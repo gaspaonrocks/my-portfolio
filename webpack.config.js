72fdab074355fc63de50155ce6826863125ff133
gaspaonrocks <gr.sacha.glgm@gmail.com>
2018-02-23 14:20:27 +0100

    minimal webpack config working

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
 const webpack = require("webpack");
 const path = require("path");
 
-const config = {
+module.exports = {
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "my-portfolio.bundle.js"
   },
   module: {
-    rules: [{ test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" }]
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|woff|woff2|eot)$/,
+        use: "url-loader"
+      },
+      {
+        test: /.html$/,
+        use: "html-loader"
+      }
+    ]
   },
-  plugins: [new webpack.optimize.UglifyJsPlugin()]
+  plugins: [new webpack.optimize.UglifyJsPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000
+  }
 };
-
-module.exports = config;
